6bfced2b42873d269f85803e1cf15050f0e8cbdc
Will <Will@10.0.0.46>
2022-03-24 17:24:50 -0600

    exercise 3

--- Chapter8/src/exercise3/Car.java
+++ Chapter8/src/exercise3/Car.java
@@ -1,5 +1,28 @@
 package exercise3;
 
-public class Car {
+
+
+
+
+public class Car extends Vehicle {
+
+	private String bodyType;
+	
+	public Car(int doors, int accel, String manu, String c, String trans, String body, boolean t) {
+			super(doors, accel, manu, c, trans, t);
+	 
+	bodyType = body;
+	
+	
+	}
+
+
+	public String getBodyType() {
+		return(bodyType);
+	}
+
+
+	
+
 
 }
--- Chapter8/src/exercise3/Vehicle.java
+++ Chapter8/src/exercise3/Vehicle.java
@@ -2,4 +2,49 @@ package exercise3;
 
 public class Vehicle {
 
+	private int doorNumber, acceleration;
+	private String manufacturer, transmission, color;
+	private boolean turbo;
+	
+	public Vehicle (int doors, int accel, String manu, String c, String trans, boolean t) {
+		
+		doorNumber = doors;
+		acceleration = accel;
+		manufacturer = manu;
+		color = c;
+		turbo = t;
+		transmission = trans;
+	}
+	
+	public int getDoorNumber() {
+		return(doorNumber);
+	}
+	
+	public int getAcceleration() {
+		return(acceleration);
+	}
+	
+	public String getManufacturer() {
+		return(manufacturer);
+	}
+	
+	public String getColor() {
+		return(color);
+	}
+	
+	public boolean getTurbo() {
+		return(turbo);
+	}
+	
+	public String getTransmission() {
+		return(transmission);
+	}
+	
+	
+	
+	
+	
+	
+	
+	
 }

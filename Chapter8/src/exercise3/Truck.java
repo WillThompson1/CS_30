33bc51155352e7308b3f7663ba6c48fc7b0333f0
Will <Will@10.0.0.46>
2022-03-27 23:44:23 -0600

    working on exercise 3

--- Chapter8/src/exercise3/Truck.java
+++ Chapter8/src/exercise3/Truck.java
@@ -1,5 +1,30 @@
 package exercise3;
 
-public class Truck {
+public class Truck extends Vehicle{
 
+	private String BedSize;
+	
+	public Truck(int doors, int accel, String manu, String c, String trans, String Bed, boolean t) {
+		super(doors, accel, manu, c, trans, t);
+	
+		BedSize = Bed;
+	}
+
+	public String getBedSize() {
+		return(BedSize);
+	}
+
+	public String getBodyTypeString() {
+		
+		if (BedSize.equalsIgnoreCase("short")) {
+			return("truck has a short bed");
+		}
+		if (BedSize.equalsIgnoreCase("standard")) {
+			return("truck has a standard bed");
+		}
+		else if (BedSize.equalsIgnoreCase("long")) {
+			return("truck has a long bed");
+		} return null;
+	}
+	
 }

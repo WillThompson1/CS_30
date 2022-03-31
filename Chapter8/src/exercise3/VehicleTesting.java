8cd0afe0b9bb9c1774886a80f8711dc3b8653903
904342004 <904342004@L815WD220025.edu.cbe.ab.ca>
2022-03-31 09:57:34 -0600

    update

--- Chapter8/src/exercise3/VehicleTesting.java
+++ Chapter8/src/exercise3/VehicleTesting.java
@@ -4,7 +4,7 @@ public class VehicleTesting {
 
 	public static void main(String[] args) {
 		
-		Car G37 = new Car(4, 5.4, "Infiniti", "White", "Automatic", "Sedan", false);	
+		Car G37 = new Car(4, 5.4, "Infiniti", "White", "Automatic", "Sedan", false);
 		
 		
 	}

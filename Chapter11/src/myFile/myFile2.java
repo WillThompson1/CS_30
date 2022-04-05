c6d3b94433e4ce3569e6c407d5a6409d9381536a
904342004 <904342004@L815WD220025.edu.cbe.ab.ca>
2022-04-05 09:27:48 -0600

    myfile 2

--- Chapter11/src/myFile/myFile2.java
+++ Chapter11/src/myFile/myFile2.java
@@ -4,20 +4,37 @@
 package myFile;
 
 import java.io.*;
+import java.util.Scanner;
 
 public class myFile2 {
 
 	public static void main(String[] args) {
 	
-	File textFile = new File("C:\\users\\904342004\\supplies");
-	
+	File textFile = new File("C:\\users\\904342004\\supplies\\zzz.txt");
+	try {
 	textFile.createNewFile();
 	System.out.println("new file created");
-	} catch (IOException e) {
-		System.out.println("error creating file");
-		System.err.println("IOException: " + e.getMessage());
+		} catch (IOException e) {
+	System.out.println("error creating file");
+	System.err.println("IOException: " + e.getMessage());
+	}
 	
+	Scanner input = new Scanner(System.in);
 	
+	System.out.println("Would you like to keep or delete the file?");
+	String i = input.next();
+	
+	if (i.equalsIgnoreCase("keep")) {	
+		
+	} else if (i.equalsIgnoreCase("delete")) {
+		
+		if (textFile.delete()) {
+		System.out.println("Deleted the file: " + textFile.getName());
+		} else {
+		System.out.println("Was unable to delete the file: " + textFile.getName());	
+		}
+		
+	}
 	
 	
 	

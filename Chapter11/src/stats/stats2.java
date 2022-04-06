d89abd5bccea105c817213f98468e4079a2e05b5
904342004 <904342004@L815WD220025.edu.cbe.ab.ca>
2022-04-06 11:11:22 -0600

    stats2

--- Chapter11/src/stats/stats2.java
+++ Chapter11/src/stats/stats2.java
@@ -7,46 +7,65 @@ public class stats2 {
 	
 	public static void main(String[] args) {
 	
-	String name; 
+	FileWriter out;
+	BufferedWriter writefile;
+	String Fname; 
+	String Sname;
 	int amt;
+	double grade;
 	
 	Scanner input = new Scanner(System.in);
 	
 	System.out.println("What would you like to name the new file?");	
-	name = input.next();	
+	Fname = input.next();	
 	
 	System.out.println("How many students will be entered?");	
 	amt = input.nextInt();
 	
-	File textFile = new File("C:\\users\\904342004\\supplies\\" + name);
+	File dataFile = new File("C:\\users\\904342004\\supplies\\" + Fname);
 	try {
-		textFile.createNewFile();
+		dataFile.createNewFile();
 		System.out.println("new file created");
 		
 	} catch (IOException e) {
 		System.out.println("error creating file");
 		System.err.println("IOException: " + e.getMessage());
 		}
-		
+	
+	
+	
 	do {
 		
+		try {
 		
+		
+	
+		System.out.println("Enter name of student:");	
+		Sname = input.next();
+		Sname+=input.nextLine();	
+		
+		
+		System.out.println("Enter " + Sname + "'s grade");	
+		grade = input.nextInt();
 		
 		
 		amt = amt - 1;
 		
-	} while (amt > 0);
+		} catch (IOException e) {
+			
+			System.out.println("Encountered a problem writing to file");
+			System.err.println("IOException: " + e.getMessage());
+			
+			}
 	
-	
-	}
-		
-		
-		
-		
-		
-		
-		
 		
 	}
 	
-}
+	 while (amt > 0);
+	} 
+	}
+
+	
+	
+
+

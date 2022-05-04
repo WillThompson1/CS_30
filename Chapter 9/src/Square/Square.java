1a1247392ea3618d5ee70233b92205fe4683d047
904342004 <904342004@L815WD220025.edu.cbe.ab.ca>
2022-05-04 12:00:19 -0600

    chapter 9 doesnt work

--- Chapter 9/src/Square/Square.java
+++ Chapter 9/src/Square/Square.java
@@ -2,4 +2,19 @@ package Square;
 
 public class Square {
 
+	public static void main(String[] args) {
+		
+	
+	int[] Squares = new int[5];
+
+		
+		
+	
+	
+
+	System.out.println(Squares);
+	
+	
+	
+	}
 }

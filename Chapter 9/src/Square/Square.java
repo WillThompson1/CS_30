1145bcc35e186598a4a07d3e8f2293aedfbb304d
904342004 <904342004@L815WD220025.edu.cbe.ab.ca>
2022-05-05 09:35:43 -0600

    finished squares

--- Chapter 9/src/Square/Square.java
+++ Chapter 9/src/Square/Square.java
@@ -3,16 +3,20 @@ package Square;
 public class Square {
 
 	public static void main(String[] args) {
-		
 	
-	int[] Squares = new int[5];
+	int index = 0;
+	int length = 5;
+	
+	int[] Squares = {0, 1, 2, 3, 4};
 
-		
-		
+	for (index = 0; index < length; index++) {
+		Squares[index] = ((int) Squares[index] * Squares[index]);
+	}
+		System.out.println("Squares");
 	
-	
-
-	System.out.println(Squares);
+		for (int element: Squares) {
+		System.out.println(element);
+	}
 	
 	
 	

aeca44b113ee81f22ac08848865ddbf8e17944b7
Will <Will@10.0.0.46>
2022-05-10 20:50:31 -0600

    working on highestgrade

--- Chapter 9/src/HighestGrade/HighestGrade.java
+++ Chapter 9/src/HighestGrade/HighestGrade.java
@@ -1,5 +1,61 @@
 package HighestGrade;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class HighestGrade {
 
+	public static void main(String[] args) {
+	
+		Scanner input = new Scanner(System.in);	
+	ArrayList<Integer> numbers = new ArrayList<Integer>();	
+	int i;
+	
+	for(i=0; i<5; i++) { 
+		
+		    System.out.println( "Enter number : " ); 
+		    int InputNumber = input.nextInt();
+		    numbers.add(InputNumber);
+		  } 	
+		
+		input.close();
+		
+	for(int o=0; o<5; o++) { 
+			
+		   Integer element1 = numbers.get(o);	//element one is set as the integer at the 'o' which will increase with the for statement
+		   Integer element2 = numbers.get(o+1);	 //element two is set as one higher than the first element to compare element one with the next integer in the list
+		   if (element1.compareTo(element2) == 0) {
+			   
+			   
+		   }
+		   
+		   
+		   
+		   
+		   
+	
+	
+	} 		
+	
+		
+		
+		
+		
+		
+		
+	}
+ 	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }

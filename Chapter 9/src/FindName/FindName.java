8ba1e4a93d1c633ea1e07b073f73bedec3ac788f
Will <Will@10.0.0.46>
2022-05-22 14:38:17 -0600

    finished comments on findname

--- Chapter 9/src/FindName/FindName.java
+++ Chapter 9/src/FindName/FindName.java
@@ -17,35 +17,29 @@ public class FindName {
 	
 	
 	
-	System.out.print("How many students would you like to enter");	
+	System.out.print("How many students would you like to enter");	//prompts the user for the amount of students the array needs to hold
 	amt = input.nextInt();	
 	
 	
-	String [] NameArray = new String[amt];	
+	String [] NameArray = new String[amt];	//creates an array with the user entered amount of strings 
 		
 	
 	
-	for (y=0; y<NameArray.length; y++){
+	for (y=0; y<NameArray.length; y++){	//while y is less than the length of the array (y increases each time this is ran)
 	
 		System.out.print("enter students name");
-        SName = input.nextLine();
-       
+        SName = input.nextLine();	//the program will assign the user entered name to the Sname variable then add it to the array
+        							//the index of the array at the point the string is entered is the y value 
         NameArray[y]= SName;
    }
 	
 	
 	
-	
-	
-	
+	//searches the array for the name 
 	
     System.out.print("what name would you like to find?: ");
     search = input.nextLine();
     
-    
-    
-    
-   
     location = Search.Linear(NameArray, search);
     
     

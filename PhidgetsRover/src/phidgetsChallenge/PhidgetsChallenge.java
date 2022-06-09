4e162cbaba8348d82af7dc6a8ac0ec2f7f32132d
904342004 <904342004@L815WD220025.edu.cbe.ab.ca>
2022-06-09 10:26:25 -0600

    challenge not done

--- PhidgetsRover/src/phidgetsChallenge/PhidgetsChallenge.java
+++ PhidgetsRover/src/phidgetsChallenge/PhidgetsChallenge.java
@@ -18,110 +18,121 @@ public class PhidgetsChallenge {
 	        leftMotors.setHubPort(5);
 	        leftMotors.setChannel(0);
 	        rightMotors.setHubPort(5);
-	        rightMotors.setChannel(1);
-	        sonar.setHubPort(3);
-	        
+	        rightMotors.setChannel(1); 
 	        
 
 	        //Open
 	        leftMotors.open(5000);
 	        rightMotors.open(5000);
 	        sonar.open(5000);
+	        sonar.setDataInterval(100);
 	        
 	        while (true) {
 	        
-	        if (sonar.getDistance() < 400) {
-	        		 
-	        	leftMotors.setTargetVelocity(-1);
-		        rightMotors.setTargetVelocity(-1);
-	        	Thread.sleep(400);
-	        	
-	        	leftMotors.setTargetVelocity(0);
+	        System.out.println("Distance:" + sonar.getDistance() + " mm");
+	        
+	        while (sonar.getDistance() > 550) {
+	        	 
+	        	 //increasing acceleration  	 
+		        leftMotors.setAcceleration(leftMotors.getMaxAcceleration());
+		 	    rightMotors.setAcceleration(rightMotors.getMaxAcceleration());
+		 	      	 
+		        //Move forward at full speed
+		        leftMotors.setTargetVelocity(-1);
+		        rightMotors.setTargetVelocity(1);
+
+		        //1.62 second pause
+		        Thread.sleep(1502);
+
+		        leftMotors.setTargetVelocity(0);
 		        rightMotors.setTargetVelocity(0);
-	        		 
-	         } else {
-	        	
-	        //increasing acceleration  	 
-	        leftMotors.setAcceleration(leftMotors.getMaxAcceleration());
-	 	    rightMotors.setAcceleration(rightMotors.getMaxAcceleration());
-	 	      	 
-	        //Move forward at full speed
-	        leftMotors.setTargetVelocity(-1);
-	        rightMotors.setTargetVelocity(1);
-
-	        //1.62 second pause
-	        Thread.sleep(1502);
-
-	        leftMotors.setTargetVelocity(0);
-	        rightMotors.setTargetVelocity(0);
-	        
-	        Thread.sleep(1200);
-	        
-	        //turn right
-	        leftMotors.setTargetVelocity(1);
-	        rightMotors.setTargetVelocity(1);
-	        
-	        //0.59 second pause
-	        Thread.sleep(220);
-	        
-	        //Move forward at full speed
-	        leftMotors.setTargetVelocity(-1);
-	        rightMotors.setTargetVelocity(1);
-	        
-	        //1.62 second pause
-	        Thread.sleep(1502);
-	        
-	        leftMotors.setTargetVelocity(0);
-	        rightMotors.setTargetVelocity(0);
-	        
-	        Thread.sleep(1200);
-	        
-	        //turn right
-	        leftMotors.setTargetVelocity(1);
-	        rightMotors.setTargetVelocity(1);
-	        
-	        //0.59 second pause
-	        Thread.sleep(215);
-	        
-	        //Move forward at full speed
-	        leftMotors.setTargetVelocity(-1);
-	        rightMotors.setTargetVelocity(1);
-	        
-	        //1.62 second pause
-	        Thread.sleep(1502);
-	        
-	        leftMotors.setTargetVelocity(0);
-	        rightMotors.setTargetVelocity(0);
-	        
-	        Thread.sleep(1200);
-	        
-	        //turn right
-	        leftMotors.setTargetVelocity(1);
-	        rightMotors.setTargetVelocity(1);
-	        
-	        //0.59 second pause
-	        Thread.sleep(210);
-	        
-	        //Move forward at full speed
-	        leftMotors.setTargetVelocity(-1);
-	        rightMotors.setTargetVelocity(1);
-	        
-	        //1.62 second pause
-	        Thread.sleep(1450);
-	        
+		        
+		        Thread.sleep(1200);
+		        
+		        //turn right
+		        leftMotors.setTargetVelocity(1);
+		        rightMotors.setTargetVelocity(1);
+		        
+		        //0.220 second pause
+		        Thread.sleep(220);
+		        
+		        //Move forward at full speed
+		        leftMotors.setTargetVelocity(-1);
+		        rightMotors.setTargetVelocity(1);
+		        
+		        //1.62 second pause
+		        Thread.sleep(1502);
+		        
+		        leftMotors.setTargetVelocity(0);
+		        rightMotors.setTargetVelocity(0);
+		        
+		        Thread.sleep(1200);
+		        
+		        //turn right
+		        leftMotors.setTargetVelocity(1);
+		        rightMotors.setTargetVelocity(1);
+		        
+		        //0.215 second pause
+		        Thread.sleep(215);
+		        
+		        //Move forward at full speed
+		        leftMotors.setTargetVelocity(-1);
+		        rightMotors.setTargetVelocity(1);
+		        
+		        //1.502 second pause
+		        Thread.sleep(1502);
+		        
+		        leftMotors.setTargetVelocity(0);
+		        rightMotors.setTargetVelocity(0);
+		        
+		        Thread.sleep(1200);
+		        
+		        //turn right
+		        leftMotors.setTargetVelocity(1);
+		        rightMotors.setTargetVelocity(1);
+		        
+		        //0.59 second pause
+		        Thread.sleep(210);
+		        
+		        //Move forward at full speed
+		        leftMotors.setTargetVelocity(-1);
+		        rightMotors.setTargetVelocity(1);
+		        
+		        //1.62 second pause
+		        Thread.sleep(1450);
+		        
+		        //stopping both motors
+		        leftMotors.setTargetVelocity(0);
+		        rightMotors.setTargetVelocity(0);
+		       
+		        Thread.sleep(1000);
 	      
 	        
-	        //stopping both motors
-	        leftMotors.setTargetVelocity(0);
-	        rightMotors.setTargetVelocity(0);
-	         }
+	        } while (sonar.getDistance() < 550) {
+		       	 
+		            leftMotors.setTargetVelocity(0);
+		        	rightMotors.setTargetVelocity(0); 
+		        	Thread.sleep(1000);
+		        		    
+		            leftMotors.setTargetVelocity(-1);
+		        	rightMotors.setTargetVelocity(-1);
+		            Thread.sleep(900);
+		                	
+		            leftMotors.setTargetVelocity(0);
+		        	rightMotors.setTargetVelocity(0);
+		          
+	        
+	        }
+	          
+	        
+	        Thread.sleep(100);
+	        
+	       }
+	 }
+
 }
-}
-}
-	  
 	
-	
-	
+
 
 	
 
